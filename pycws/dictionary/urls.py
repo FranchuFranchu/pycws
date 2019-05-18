b851b45a75e86e44d97aa224ac1c8875d01520fa
KathTheDragon <kaththedragon@gmail.com>
2019-05-18 16:38:07 +0100

    Expand dictionary urls/views

--- pycws/dictionary/urls.py
+++ pycws/dictionary/urls.py
@@ -5,4 +5,28 @@ app_name = 'dictionary'
 
 urlpatterns = [
     path('', views.view_dict, name='view-dict'),
+    path('new_word/', views.new_word, name='new-word'),
+    path('word/<lemma>/', include([
+        path('', views.view_word, name='view-word'),
+        path('edit/', views.edit_word, name='edit-word'),
+        path('delete/', views.delete_word, name='delete-word'),
+        path('<int:hnum>/', include([
+            path('', views.view_word, name='view-word-hnym'),
+            path('edit/', views.edit_word, name='edit-word-hnym'),
+            path('delete/', views.delete_word, name='delete-word-hnym'),
+        ])),
+    ])),
+    path('settings/', views.settings, name='settings'),
+    path('import/', views.import, name='import'),
+    path('export/', views.export, name='export'),
+    path('mass_edit/', views.mass_edit, name='mass-edit'),
+    path('auto_match/', views.auto_match, name='auto-match'),
+    path('maintenance/', views.maintenance, name='maintenance'),
+    path('purge/', views.purge, name='purge'),
+    path('homonyms/', views.homonyms, name='homonyms'),
+    path('classes/', include([
+        path('', views.classes, name='classes'),
+        path('distribution/', views.class_distribution, name='class-distribution'),
+    ])),
+    path('roots/', views.roots, name='roots'),
 ]
--- pycws/dictionary/views.py
+++ pycws/dictionary/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
 from django.http import Http404
 
+# The dictionary views must include an argument `code` for the language code
+
 # Create your views here.
 def view_dict(request, code):
+    raise Http404
+
+def new_word(request, code):
+    raise Http404
+
+def view_word(request, code, lemma, hnum=0):
+    raise Http404
+
+def edit_word(request, code, lemma, hnum=0):
+    raise Http404
+
+def delete_word(request, code, lemma, hnum=0):
+    raise Http404
+
+def settings(request, code):
+    raise Http404
+
+def import(request, code):
+    raise Http404
+
+def export(request, code):
+    raise Http404
+
+def mass_edit(request, code):
+    raise Http404
+
+def auto_match(request, code):
+    raise Http404
+
+def maintenance(request, code):
+    raise Http404
+
+def purge(request, code):
+    raise Http404
+
+def homonyms(request, code):
+    raise Http404
+
+def classes(request, code):
+    raise Http404
+
+def class_distribution(request, code):
+    raise Http404
+
+def roots(request, code):
     raise Http404
