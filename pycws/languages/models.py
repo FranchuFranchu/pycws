6649fcd5c478f72ca963fa474a912c480771c515
KathTheDragon <kaththedragon@gmail.com>
2019-05-18 21:35:44 +0100

    Add dialect to languages

--- pycws/languages/models.py
+++ pycws/languages/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Language(models.Model):
     code = models.CharField(unique=True, max_length=6)
     name = models.TextField()
+
+class Dialect(models.Model):
+    lang = models.ForeignKey(Language, on_delete=models.CASCADE)
